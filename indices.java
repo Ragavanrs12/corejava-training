f0013aa0f716750d51c6bc3b7dddbee85f152f6b
Ragavanrs12 <74526179+Ragavanrs12@users.noreply.github.com>
2020-11-16 14:50:32 +0530

    Update indices.java

--- indices.java
+++ indices.java
@@ -13,15 +13,13 @@ public class indices {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner obj=new Scanner(System.in);
-		char[] name=obj.next().toCharArray();
+		System.out.println("enter the string");
+		String name=obj.nextLine();
 		System.out.println("enter the two indices to be displayed");
-		int a=obj.nextInt();
-		int b=obj.nextInt();
-		for(int i=a;i<b;i++)
-			System.out.print(name[i]);
+		int start=obj.nextInt();//starting indices
+		int end=obj.nextInt();//ending indices
+			System.out.print(name.substring(start,end));
 		obj.close();
-		
 	}
 
 }
-
